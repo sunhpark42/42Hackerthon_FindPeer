64457e0f0ff9e9317907fbc819f25022da73d66b
Sunhee Park <sunheepark@Sunheeui-MacBookPro-2.local>
2020-12-16 15:21:57 +0900

    initial commit

---
+++ Shared/FindPeer42App.swift
@@ -0,0 +1,17 @@
+//
+//  FindPeer42App.swift
+//  Shared
+//
+//  Created by Sunhee Park on 2020/12/16.
+//
+
+import SwiftUI
+
+@main
+struct FindPeer42App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
