8de41834d5e0bae2298f8190d3a184e8c8430de6
Sunhee Park <sunheepark@Sunheeui-MacBookPro-2.local>
2020-12-16 17:05:09 +0900

    initial commit

---
+++ FindPeers42/FindPeers42/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindPeers42
+//
+//  Created by Sunhee Park on 2020/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+#Find Peers!
+
+현재 진행중인 과제, 혼자하기 힘들다면 이미 끝낸 사람에게 물어보거나 같이 할 동료를 찾아보아요.
+
+### 개발
+sunhpark, hyulee, njeong
+
+### 사용언어
+<img src="https://img.shields.io/badge/swift-v5.1-orange?logo=swift" />
